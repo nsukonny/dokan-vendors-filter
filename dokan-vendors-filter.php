0e4337aca06b24775747c8897c8e04d84cf35a08
Nickolay Sukonny <kajenick@gmail.com>
2019-08-11 12:13:51 +0300

    First commit

---
+++ dokan-vendors-filter.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Dokan Vendors Filter
+Plugin URI: http://nsukonny.ru/dokan-vendor-filter
+Description: Display vendors list with filters
+Version: 1.0.0
+Author: nSukonny
+Author URI: http://nsukonny.ru
+License: A "Slug" license name e.g. GPL2
+*/
